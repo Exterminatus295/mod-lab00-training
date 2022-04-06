4958b0e80fa406cd294d95f771764247d8280990
Exterminatus295 <81161457+Exterminatus295@users.noreply.github.com>
2022-04-06 11:59:23 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,10 +1,10 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
-int sum, x, n;
+int a, x, n;
 int64_t power(int64_t x, uint16_t n) {
-    sum = 1;
+    a = 1;
     for (int i = 0; i < n; i++) {
-        sum *= x;
+        a *= x;
     }
-    return sum;
+    return a;
 }
